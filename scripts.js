afa5869ba3d873917f5d78282fd27b8d491b0beb
Raja <Lavanya.Raja@us.gt.com>
2025-05-16 06:00:02 -0500

    updated html

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-console.log('init');
-
-const  main = () => {
-    console.log('main');
-}
-
-document.addEventListener('DOMContentLoaded', main);
-
+document.querySelector("img.main-header-brightness").addEventListener("click", function() {
+    document.querySelector(".main").classList.toggle("background-updated");
+ });
+console.log("hello");
+ document.querySelector(".main-content-inputbox").addEventListener("keyup", function(){
+    document.querySelector(".charCount").textContent = this.value.length;
+    document.querySelector(".wordCount").textContent = this.value.split(/\s+/).length-1;
+    document.querySelector(".sentenceCount").textContent = this.value.split(/\./).length-1;
+});
